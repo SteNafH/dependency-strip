2a6e09e1eb6e731b0ddfa04f5205ada2f7c3c9ac
SteNafH <stefanh2005@gmail.com>
2023-05-13 15:53:14 +0200

    Working build

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    silent: false,
-    verbose: false,
-    collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.ts"],
-};
